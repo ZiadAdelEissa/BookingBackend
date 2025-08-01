36b77858789f30083db7add06cebeaeb00713432
ZiadAdelEissa <ziadadel12321@hotmail.com>
2025-08-02 01:12:35 +0300

    add origins

--- index.js
+++ index.js
@@ -72,12 +72,18 @@ app.use(
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174", 
+      "http://localhost:3000",
+      process.env.FRONTEND_URL
+    ].filter(Boolean), // Remove any undefined values
     methods: ["GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
