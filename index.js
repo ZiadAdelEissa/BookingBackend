a2d717cb17163dc98a3ca16809466a92bf38e5c4
ZiadAdelEissa <ziadadel12321@hotmail.com>
2025-08-02 01:54:41 +0300

    fixs

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.use(
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5174",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
